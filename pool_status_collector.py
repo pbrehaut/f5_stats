96d676b5e714d2c6c3ebb5aefde87bb4f7aa63dd
pbrehaut <pbrehaut@hotmail.com>
2026-02-05 09:11:27 +1100

    Add menu interface and refactor data collection functions

--- pool_status_collector.py
+++ pool_status_collector.py
@@ -60,7 +60,12 @@ def get_hostname():
 
 
 def get_ltm_pool_members():
-    output = run_command("tmsh show ltm pool members field-fmt")
+    output = run_command("tmsh show ltm pool /*//* members field-fmt")
+    return parse_f5_config(output)
+
+
+def get_ltm_virtual_stats():
+    output = run_command("tmsh show ltm virtual /*//* field-fmt")
     return parse_f5_config(output)
 
 
@@ -69,30 +74,83 @@ def get_gtm_pool_members():
     return parse_f5_config(output)
 
 
-def save_pool_data(data, chassis_id, hostname, date_part, time_part, pool_type):
-    filename = "/var/tmp/{}__{}__{}__{}__{}_pool_members.json".format(
-        chassis_id, hostname, date_part, time_part, pool_type
+def save_data(data, chassis_id, hostname, date_part, time_part, data_type):
+    filename = "/var/tmp/{}__{}__{}__{}__{}.json".format(
+        chassis_id, hostname, date_part, time_part, data_type
     )
     with open(filename, 'w') as f:
         json.dump(data, f, indent=2)
     print("Saved to: {}".format(filename))
 
 
+def display_menu():
+    print("\nF5 Data Collection Menu")
+    print("-" * 30)
+    print("1. LTM Pool Members")
+    print("2. LTM Virtual Server Stats")
+    print("3. GTM Pool Members")
+    print("4. All LTM Data (Pools + Virtuals)")
+    print("5. All Data")
+    print("6. Exit")
+    print("-" * 30)
+
+
+def get_user_choice():
+    while True:
+        display_menu()
+        choice = input("Enter your choice (1-6): ").strip()
+        if choice in ['1', '2', '3', '4', '5', '6']:
+            return choice
+        print("Invalid choice. Please enter a number between 1 and 6.")
+
+
+def collect_ltm_pools(chassis_id, hostname, date_part, time_part):
+    try:
+        ltm_data = get_ltm_pool_members()
+        save_data(ltm_data, chassis_id, hostname, date_part, time_part, "ltm_pool_members")
+    except Exception as e:
+        print("No LTM pools found or error retrieving LTM pool members: {}".format(e))
+
+
+def collect_ltm_virtuals(chassis_id, hostname, date_part, time_part):
+    try:
+        virtual_data = get_ltm_virtual_stats()
+        save_data(virtual_data, chassis_id, hostname, date_part, time_part, "ltm_virtual_stats")
+    except Exception as e:
+        print("No LTM virtuals found or error retrieving LTM virtual stats: {}".format(e))
+
+
+def collect_gtm_pools(chassis_id, hostname, date_part, time_part):
+    try:
+        gtm_data = get_gtm_pool_members()
+        save_data(gtm_data, chassis_id, hostname, date_part, time_part, "gtm_pool_members")
+    except Exception as e:
+        print("No GTM pools found or error retrieving GTM pool members: {}".format(e))
+
+
 if __name__ == '__main__':
+    choice = get_user_choice()
+
+    if choice == '6':
+        print("Exiting.")
+        exit(0)
+
     chassis_id = get_chassis_id()
     hostname = get_hostname()
     now = datetime.now()
     date_part = now.strftime("%Y-%m-%d")
     time_part = now.strftime("%H-%M-%S")
 
-    try:
-        ltm_data = get_ltm_pool_members()
-        save_pool_data(ltm_data, chassis_id, hostname, date_part, time_part, "ltm")
-    except:
-        print("No LTM pools found or error retrieving LTM pool members.")
-
-    try:
-        gtm_data = get_gtm_pool_members()
-        save_pool_data(gtm_data, chassis_id, hostname, date_part, time_part, "gtm")
-    except:
-        print("No GTM pools found or error retrieving GTM pool members.")
+    if choice == '1':
+        collect_ltm_pools(chassis_id, hostname, date_part, time_part)
+    elif choice == '2':
+        collect_ltm_virtuals(chassis_id, hostname, date_part, time_part)
+    elif choice == '3':
+        collect_gtm_pools(chassis_id, hostname, date_part, time_part)
+    elif choice == '4':
+        collect_ltm_pools(chassis_id, hostname, date_part, time_part)
+        collect_ltm_virtuals(chassis_id, hostname, date_part, time_part)
+    elif choice == '5':
+        collect_ltm_pools(chassis_id, hostname, date_part, time_part)
+        collect_ltm_virtuals(chassis_id, hostname, date_part, time_part)
+        collect_gtm_pools(chassis_id, hostname, date_part, time_part)
